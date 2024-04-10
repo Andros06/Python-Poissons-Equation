38bc5484ba989a5e03f7b5ad7fe6dc7e06831481
Andreas Roset <andreasroset@gmail.com>
2024-04-10 20:01:43 +0200

    modified:   Poisson_Dirichlet.py 	modified:   Poisson_Neumann.py

--- Poisson_Dirichlet.py
+++ Poisson_Dirichlet.py
@@ -3,42 +3,47 @@ import scipy.sparse.linalg as lin
 import numpy as np
 import matplotlib.pyplot as plt
 
-# antall punkter
+# Antall punkter, N inkluderer star og slutt punkt
 m = 5
 N = m + 2
 
-#s tart og slutt punkt
+# Start og slutt punkt (Fra punkt a til punkt b)
 Pa = -1 
 Pb = 1
 
-# Setter verdier i start og slutt punkt
+# Start og slutt punkt verdier (Verdi for punkt a og b)
 PaV = 0
 PbV = 2
 
-# Danner X
+# Lager x verdier mellom Pa og Pb med N antall punkt
 x = np.linspace(Pa, Pb, N)
 
-# Avstand mellom punktene
+# Finner avstand mellom punktene
 h = x[1] - x[0]
 
+# Lager sparsmatrise med andrederivert ved "Finitedifference-metode"
 L = (1/h**2) * sp.diags([1, -2, 1], [-1, 0, 1], shape=(m,m))
 
+# Konverterar til vanlig matrise
 A = L.toarray()
 
+# Lager til eit array med berre x verdien vi vil finne
 x_intern = x[1:-1]
 
+# Funksjon fra oppgåven
 F = np.cos(np.pi * x_intern)
 
-
+# Legger til Dirichlet randbetingelser i F
 F[0] = F[0] - (PaV / (h**2))
 F[-1]= F[-1] - (PbV / (h**2))
 
+# Løyser for A og F
 u = np.linalg.solve(A, F)
 
+# Legger til Dirichlet randbetingelser
 u_full = np.hstack([PaV, u, PbV])
 
-print(u_full)
-
+# Plotting for graf
 plt.plot(x, u_full)
 plt.xlabel("x")
 plt.ylabel("u(x)")
--- Poisson_Neumann.py
+++ Poisson_Neumann.py
@@ -3,49 +3,56 @@ import scipy.sparse.linalg as lin
 import numpy as np
 import matplotlib.pyplot as plt
 
-#antall punkter
+# Antall punkter, N inkluderer star og slutt punkt
 m = 4
 N = m + 2
 
-#start og slutt punkt (Point a)
+# Start og slutt punkt (Fra punkt a til punkt b)
 Pa = -1 
 Pb = 1
 
-#start og slutt punkt verdier (Point a Value)
+# Start og slutt punkt verdier (Verdi for punkt a og b)
 PaV = 1
 PbV = 1
 
+# Lager x verdier mellom Pa og Pb med N antall punkt
 x = np.linspace(Pa, Pb, N)
 
-# avstand mellom punktene
+# Finner avstand mellom punktene
 h = x[1] - x[0]
 
+# Lager til den andrederiverte matrisen L2 
+# Lager først sparsmatrise med andrederivert ved "Finitedifference-metode"
 L2 = (1/h**2) * sp.diags([1, -2, 1], [-1, 0, 1], shape=(N,N))
 
+# Konvertere til komprimert matrise for effektivitet
 L2 = sp.csr_matrix(L2)
 
 
-#Venstre randbetingelser
+# Venstre randbetingelser
 L2[0, 0] = -1/h
 L2[0, 1] =  1/h
 
-#Høgre randbetingelser
+# Høgre randbetingelser
 L2[-1, -1] = 1/h
 L2[-1, -2] = -1/h
 
+# Omega = 0 pga poisson likning
 omega = 0
 
 B = L2 + (omega**2) * sp.eye(N)
 
+# Funksjonen fra oppgåve
 G = np.cos(np.pi * x)
 
 # setter inn randbetingelsen i vektoren G
 G[0]  =  PaV
 G[-1] =  PbV
 
-# Løyser likninga
+# Løyser likningen 
 V, istop, itn, r1norm = lin.lsqr(L2, G)[:4]
 
+# Plotting for graf
 plt.plot(x, V)
 plt.xlabel('x')
 plt.ylabel('u(x)')
